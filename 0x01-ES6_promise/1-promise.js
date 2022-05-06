90a6f89791d90e295fa0da7a108a61629160a6ab
Byaxy <charlesbyaxy@gmail.com>
2022-05-06 19:04:32 +0000

    promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, fail) => {
+	if (success) {
+	    resolve({
+		status: 200,
+		body: 'Success',
+	    });
+	} else {
+	    fail(new Error('The fake API is not working currently'));
+	}
+    });
+}

3c2a658ee13f8acfab6dbd4eb29f13622b1b8e59
Byaxy <charlesbyaxy@gmail.com>
2022-06-16 09:11:02 +0000

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,8 +2,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, fail) => {
     if (success) {
       resolve({
-      status: 200,
-      body: 'Success',
+        status: 200,
+        body: 'Success',
       });
     } else {
       fail(new Error('The fake API is not working currently'));
